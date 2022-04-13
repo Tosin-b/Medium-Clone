f7122c816d7c414d3d2688650e6f5e8d5ac319b5
Tosin-b <tosynbiala@outlook.com>
2022-04-13 22:24:04 +0100

    first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='flex items-center justify-between py-10 bg-yellow-400 border-black border-y lg:py-0'>
+        <div className='px-10 space-y-5'>
+            <h1 className='max-w-xl font-serif text-6xl' > <span className='underline decoration-black decoration-4'>Medium</span> is a place to write read, and connect</h1>
+            <h2> Its easy and free to post your thinking on any topic and connect with milions of readers.</h2>
+        </div>
+        <div>
+            <img className='hidden h-32 md:inline-flex lg:h-full' src='https:accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png' alt=''/>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+function Header() {
+  return (
+    <header className="flex justify-between p-5 mx-auto max-w-7xl">
+      <div className="flex items-center space-x-5 ">
+        <Link href="/posting"passHref>
+          <img
+            src="https://links.papareact.com/yvf"
+            className="object-contain cursor-pointer w-44"
+           alt=""/>
+        </Link>
+        <div className="items-center hidden space-x-5 md:inline-flex">
+          <h3>About </h3>
+          <h3> Contact</h3>
+          <h3 className="px-4 py-1 text-white bg-green-600 rounded-full">Follow</h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign in</h3>
+        <h3 className="px-4 py-1 border border-green-600 rounded-full">Get started</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
